cd0aa1a082688ff6fefcb60bed23fde036532afa
Daniel France <daniel.france@bigterra.com>
2026-02-16 22:16:30 +0100

    get_police cleanup

--- scraper/police_tools/get_police_articles.py
+++ scraper/police_tools/get_police_articles.py
@@ -11,7 +11,7 @@ from config import POLICE_ARTICLES_FP, URL_KEYWORDS, LOG_DIR, ERRORS_LOG_FP, YEA
 from scraper.args import ScraperArguments
 from scraper.core import BaseScraper
 from scraper.site_configs import POLICE_SELECTOR, BASE_POLICE_URL
-from utils.io_utils import async_json_read, atomic_json_write, CriticalDataError
+from utils.io_utils import async_json_read, atomic_json_write, CriticalDataError, read_json
 from utils.logger import LogConfig, destroy
 
 
@@ -48,14 +48,12 @@ class ListingsParser(BaseScraper):
     def __init__(self):
         super().__init__()
         self.existing_urls: set = set()
-        self.results_buffer = []
-        self.cached_results = []
 
 
     async def _read_and_write(
         self,
         municipality: str,
-        articles: list[str],
+        found_urls: list[str],
         year: int | str,
     ) -> None:
         """ I/O helper for the 'scrape_municipality' function. \n
@@ -64,25 +62,24 @@ class ListingsParser(BaseScraper):
         async with self.lock:
             data = await async_json_read(POLICE_ARTICLES_FP)
 
-        # Prepare the keys
+        # Update the keys if needed
         year_str = str(year)
         if municipality not in data:
             data[municipality] = {}
         if year_str not in data[municipality]:
             data[municipality][year_str] = []
 
-        # Append the results
-        articles = list(set(articles))  # Dedupe current scrape
-        existing_urls = set(data[municipality][year_str])  # What's already saved
-        new_urls = [url for url in articles if url not in existing_urls]
-        data[municipality][year_str].extend(new_urls)
+        found_urls = list(set(found_urls)) # Cute dedupe
+        existing_results = set(data[municipality][year_str])
+        new_urls = [url for url in found_urls if url not in existing_results]
 
+        data[municipality][year_str].extend(new_urls)
         async with self.lock:
             atomic_json_write(data, POLICE_ARTICLES_FP)
-            self.logger.info(f"Writing {len(articles)} articles from '{municipality}'/'{year}'")
+            self.logger.info(f"Writing {len(found_urls)} articles from '{municipality}'/'{year}'")
 
 
-    def process_article_results(self, article_results: list) -> ScrapeLogData:
+    def aggregate_stats(self, article_results: list) -> ScrapeLogData:
         """ Process all the article results from parsing the listings pages.
             Aggregate and return their stats.
         """
@@ -100,7 +97,6 @@ class ListingsParser(BaseScraper):
             saved_articles += articles
             articles_processed += all_articles
             total_pages += pages
-
         return ScrapeLogData(saved_articles, failed_articles, articles_processed, total_pages)
 
 
@@ -110,22 +106,20 @@ class ListingsParser(BaseScraper):
         metadata: ListingScrapeMetadata,
         year: int,
         municipality,
-        cr_max_pages,
+        cron_max_pages,
     ) -> NextUrl | None:
         next_page = main_soup.select_one(POLICE_SELECTOR['pagination']['next_page'])
+
         if next_page:
             next_page_link = next_page['href']
             next_url = BASE_POLICE_URL + next_page_link
             metadata.pages_scraped += 1
-            self.logger.debug(f"Continuing to page {metadata.pages_scraped}/"
-                              f"{metadata.listing_max_pages if not cr_max_pages else cr_max_pages} "
-                              f"in year {year} for: '{municipality}'")
+            self.logger.debug(f"Continuing to page {metadata.pages_scraped}/{metadata.listing_max_pages if not cron_max_pages else cron_max_pages} in year {year} for: '{municipality}'")
         else:
             self.logger.info(f"No next page found, checked {metadata.all_articles_count} articles")
             self.logger.info(f"Found {len(metadata.articles)} articles from "
                              f"{metadata.pages_scraped} pages in year {year} for: {municipality}...")
             next_url = None
-
         return next_url
 
 
@@ -135,7 +129,7 @@ class ListingsParser(BaseScraper):
         metadata: ListingScrapeMetadata,
         municipality: str,
         year: int,
-        cr_max_pages: int,
+        cron_max_pages: int,
     ) -> NextUrl | None:
         """ Parse the html content of the current articles listing page. \n
             Mutates the "metadata", to which it appends the URL of articles we want, as well as some stats.
@@ -156,18 +150,16 @@ class ListingsParser(BaseScraper):
 
         # Loop over each article, if we get a match -> save it
         for article in article_list:
-            # If this fail, just let it crash
-            link_el = article.select_one(POLICE_SELECTOR['listing_selectors']['article_link'])
-            article_href = link_el['href']
-            article_link = BASE_POLICE_URL + article_href
+            link_el = article.select_one(POLICE_SELECTOR['listing_selectors']['article_link']) # If this select fails just crash
+            article_link = BASE_POLICE_URL + link_el['href']
             metadata.all_articles_count += 1
-            if any(keyword in article_link for keyword in URL_KEYWORDS) and article_link not in self.existing_urls:
-                # todo add to a buffer instead
+            # Check existing results and a keyword match
+            if article_link not in self.existing_urls and any (keyword in article_link for keyword in URL_KEYWORDS):
                 metadata.articles.append(article_link)
                 self.logger.info(f"Found an article...:'{article_link}'")
 
         # Return the next page link, which will change the 'new_url' in 'scrape_municipality' to continue the loop
-        return self.get_next_page(soup, metadata, year, municipality, cr_max_pages)
+        return self.get_next_page(soup, metadata, year, municipality, cron_max_pages)
 
 
     async def scrape_municipality(
@@ -177,25 +169,25 @@ class ListingsParser(BaseScraper):
         municipality: str,
         cron_max_pages: int | None = None
     ):
-        """ Scrape the municipality for article links, write its results.
+        """Scrape one municipality for all article links coroutine.
+           Stores found urls in "art_data" then writes them all at the end.
         """
-        metadata = ListingScrapeMetadata()
+        art_data = ListingScrapeMetadata()
         new_url = url
         try:
+            # Keep changing the 'new_url' until None
             while new_url:
                 # For cron, we scrape only the required amount of pages
-                if cron_max_pages is not None and metadata.pages_scraped >= cron_max_pages:
+                if cron_max_pages is not None and art_data.pages_scraped >= cron_max_pages:
                     self.logger.info(f"Finished scraping the required cron job pages...")
                     break
-                # Keep changing the 'new_url' until no new pages are left
-                new_url = await self.parse_listing(new_url, metadata, municipality, year, cron_max_pages)
+                new_url = await self.parse_listing(new_url, art_data, municipality, year, cron_max_pages)
 
             # Write any saved articles, return the metadata about the scraper
-            if len(metadata.articles) > 0:
-                await self._read_and_write(municipality, metadata.articles, year)
-            return len(metadata.articles), metadata.pages_scraped, metadata.all_articles_count
-
-        except CriticalDataError: # Re-raise writing error
+            if len(art_data.articles) > 0:
+                await self._read_and_write(municipality, art_data.articles, year)
+            return len(art_data.articles), art_data.pages_scraped, art_data.all_articles_count
+        except CriticalDataError: # Raise writing error
             raise
         except Exception as e: # Catch any generic error
             self.logger.error(f" {municipality}/{year} failed for '{new_url}'...Error ===>")
@@ -217,23 +209,20 @@ class ListingsParser(BaseScraper):
 
 
     def mk_cron_coros(self, archives, cron_max_pages: int):
-        """ Returns coroutines of only the current year AND the
-            'non_year' archives (city/district specific, they aren't dated, we have to check them).
-        """
+        """Makes coros and defines what a "year" is."""
         current_year = str(datetime.now().year)
         article_scrape_coros = [
             self.scrape_municipality(url, year, municipality, cron_max_pages)
             for municipality, years in archives.items()
             for year, urls in years.items() if (year == current_year or year == "non_years")
-            for url in (urls if isinstance(urls, list) else [urls])  # Make single urls into a list
+            for url in (urls if isinstance(urls, list) else [urls])  # Has to be a list
         ]
 
         return article_scrape_coros
 
 
     async def set_existing_urls(self):
-        """ Used for deduping results, ie to not re-add a result which we already have.
-        """
+        """Used for deduping tasks."""
         initial_results_urls = set()
         results_dict = await async_json_read(self.OUTPUT_FILE)
         for domain, years in results_dict.items():
@@ -249,20 +238,25 @@ class ListingsParser(BaseScraper):
     async def scrape(self, cron_max_pages: int | None) -> bool:
         """Main orchestrator, runs archive jobs (to get year links of archives), then the article jobs (to get article urls).
         """
-        pre_scrape_existing_urls = await self.set_existing_urls()
+
+        # Keep a ref for the Return
+        pre_scrape_parsed_arts = await self.set_existing_urls()
         if cron_max_pages:
             self.logger.info(f"Running '{self.MODULE_NAME}' in CRON mode, pages to check: {cron_max_pages}.")
 
-        archives: dict = await async_json_read(self.INPUT_FILE)
+        archives: dict = read_json(self.INPUT_FILE)
         article_scrape_coros = self.mk_cron_coros(archives, cron_max_pages) if cron_max_pages else self.mk_full_coros(archives)
         self.logger.info(f"Scraping {len(article_scrape_coros)} tasks...")
         results = await gather(*[coro for coro in article_scrape_coros], return_exceptions=True)
 
-        logs: ScrapeLogData = self.process_article_results(results)
+        # Process the results and aggregate the stats for logs
+        logs: ScrapeLogData = self.aggregate_stats(results)
         (saved_articles, failed_articles, articles_processed, total_pages) = logs
         self.logger.info(f"Processed {articles_processed} articles from {total_pages} pages, saved {saved_articles}, failed {failed_articles}")
-        post_scrape_existing_urls = await self.set_existing_urls()
-        found_new_articles = len(pre_scrape_existing_urls) != len(post_scrape_existing_urls)
+
+        # Return true if we found any new articles
+        post_scrape_parse_arts = await self.set_existing_urls()
+        found_new_articles = len(pre_scrape_parsed_arts) != len(post_scrape_parse_arts)
         return found_new_articles
 
 
