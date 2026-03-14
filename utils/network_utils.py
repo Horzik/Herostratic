e48521b85e3bc3e466f59caa5df1b0cc79d13940
Daniel France <daniel.france@bigterra.com>
2026-03-14 03:47:43 +0100

    get_bytes response handling improvement

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -2,7 +2,7 @@ import random
 import aiohttp
 import asyncio
 
-from config import MAX_RETRIES, POPO_TIMEOUT, TIMEOUT
+from config import MAX_RETRIES, POPO_TIMEOUT, TIMEOUT, VIDEO_TIMEOUT
 from utils.logger import get_logger
 
 
@@ -12,10 +12,16 @@ class SoupError(Exception):
         super().__init__(f"Exception getting soup for url: '{url}'")
 
 class FetchError(Exception):
-    def __init__(self, url: str, status: int):
+    def __init__(self, url: str, status: int | None = None):
         self.url = url
         self.status = status
-        super().__init__(f"HTTP {status} for '{url}'")
+        super().__init__(f"HTTP {status} for '{url}'" if status else f"Fetch failed for '{url}'")
+
+class DownloadError(Exception):
+    def __init__(self, url: str, msg = ''):
+        self.url = url
+        self.msg = msg
+        super().__init__(f"Download error for '{url}'...{self.msg}")
 
 # Returns a new ClientSession with default config
 def create_session():
@@ -33,26 +39,32 @@ def create_session():
         }
     )
 
-
-
-
 # todo adaptive semaphore or other adaptive rate-limit prevention
 # todo leaky bucket ('aiolimiter', 'httpx.AsyncClient()'), TTFB (increase semaphore count OR outgoing requests)
-async def get_bytes(url: str, session: aiohttp.ClientSession, semaphore: asyncio.Semaphore, gov_site=False, verify=True
-) -> bytes | None:
+async def get_bytes(
+    url: str,
+    session: aiohttp.ClientSession,
+    semaphore: asyncio.Semaphore,
+    gov_site=False, # Longer timeout
+    verify=True, # Verify SSL certificate of the target url
+    video=False # Extra long timeout
+) -> bytes:
     """ Helper for fetching bytes.
         Retry strategy (with backoff as 'wait_time').
     """
     logger = get_logger('network')
-    timeout = POPO_TIMEOUT if gov_site else TIMEOUT
+    timeout = (
+        POPO_TIMEOUT if gov_site
+        else VIDEO_TIMEOUT if video
+        else TIMEOUT
+    )
+
     async with semaphore:
         for attempt in range(MAX_RETRIES):
             await asyncio.sleep(random.uniform(0.3, 0.5)) # Be little nice, sleep by default and wait on retries
             wait_time = 3 ** attempt
             try:
                 async with session.get(url=url, timeout=aiohttp.ClientTimeout(total=timeout), verify_ssl=verify) as response:
-
-                    # todo abstract the response handling?
                     if response.status == 200:
                         content_bytes = await response.read()
                         return content_bytes
@@ -60,35 +72,24 @@ async def get_bytes(url: str, session: aiohttp.ClientSession, semaphore: asyncio
                     # "Too many requests" ==> wait extra long
                     elif response.status == 429:
                         logger.warning(f"429 for '{url}', attempt {attempt + 1}, waiting extra long")
-                        if attempt < MAX_RETRIES - 1:
-                            jitter = random.uniform(0, wait_time)  # Add randomness
-                            await asyncio.sleep(wait_time + 10 + jitter)
-                            continue
-                        else:
-                            logger.error(f"Failed fetching '{url}' with {response.status}, all tries exhausted")
-                            raise FetchError(url, response.status)
+                        jitter = random.uniform(0, wait_time)
+                        await asyncio.sleep(wait_time + 10 + jitter)
 
                     # Retry-able errors
                     elif response.status == 500:
                         logger.warning(f"HTTP 500 for '{url}', attempt {attempt + 1}/{MAX_RETRIES}, sleeping for extra long...")
                         await asyncio.sleep(wait_time + 30)
-                        continue
-
                     elif response.status in [502, 503, 504]:
                         logger.warning(f"HTTP {response.status} for '{url}', attempt {attempt + 1}/{MAX_RETRIES}")
-                        if attempt < MAX_RETRIES - 1:
-                            jitter = random.uniform(0, wait_time)  # Add randomness
-                            await asyncio.sleep(wait_time + jitter)
-                            continue
-                        else:
-                            logger.error(f"Failed fetching '{url}' with {response.status}, all tries exhausted")
-                            raise FetchError(url, response.status)
+                        jitter = random.uniform(0, wait_time)
+                        await asyncio.sleep(wait_time + jitter)
 
-                    else:  # 404, 403, 400, etc - don't retry
+                    # 404, 403, 400, etc - don't retry
+                    else:
                         logger.error(f"HTTP {response.status} for '{url}', not retrying")
                         raise FetchError(url, response.status)
 
-            # Catch exceptions
+            # Log exceptions
             except aiohttp.ClientConnectionError as e:
                 logger.warning(f"Connection error for '{url}', (attempt {attempt + 1}/{MAX_RETRIES}):: {e}")
             except asyncio.TimeoutError as e:
@@ -98,10 +99,8 @@ async def get_bytes(url: str, session: aiohttp.ClientSession, semaphore: asyncio
 
             # Retry after exception, add a jitter
             if attempt < MAX_RETRIES - 1:
-                jitter = random.uniform(0, wait_time)  # Add randomness
+                jitter = random.uniform(0, wait_time)
                 logger.warning(f"Retrying after {wait_time + jitter:.1f}s...")
                 await asyncio.sleep(wait_time + jitter)
-            else:
-                logger.error(f"Failed fetching '{url}', all tries exhausted")
-                raise FetchError(url, 400)
-        return None
+
+        raise FetchError(url)
